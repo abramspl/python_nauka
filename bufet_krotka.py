34e25f71e99eaed10edc04ac3f249df30ebbbe7a
Łukasza Abramczyk <71088814+abramspl@users.noreply.github.com>
2020-10-08 20:18:24 +0200

    Add files via upload

---
+++ bufet_krotka.py
@@ -0,0 +1,11 @@
+bufet = ('kanapki','kawa','herbata','frytki','zupa')
+print('Aktualne menu bufetu:\n')
+for danie in bufet:
+    print(danie.title())
+
+# zastapienie aktualnej krotki nowa
+
+bufet = ('pierogi','salatka','kanapki','zupa','frytki')
+print('\nNowe menu bufetu:\n')
+for danie in bufet:
+    print(danie.title())
