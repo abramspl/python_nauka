05af1d754f89d1a13abea1dd6d980feabe092d43
Łukasza Abramczyk <71088814+abramspl@users.noreply.github.com>
2020-10-25 08:27:35 +0100

    program z zaimportowana funkcja koszulka_funkcja

---
+++ koszulka_program.py
@@ -0,0 +1,21 @@
+import koszulka_funkcja
+
+koszulka_funkcja.koszulka('xl','Dzien dobry')
+koszulka_funkcja.koszulka('m','gry planszowe')
+
+from koszulka_funkcja import koszulka
+
+koszulka('xxl','python')
+
+from koszulka_funkcja import koszulka as ka
+
+ka('xs','funkcje python')
+
+import koszulka_funkcja as kf
+
+kf.koszulka('xl','Dzien dobry')
+kf.koszulka('m','gry planszowe')
+
+from koszulka_funkcja import *
+
+koszulka('xxl','python')
