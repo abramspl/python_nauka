815f3a45ba29bac803445429ce8fef03e35ae897
Łukasza Abramczyk <71088814+abramspl@users.noreply.github.com>
2020-10-25 08:16:49 +0100

    program z zaimportowana funkcja printing_function

---
+++ print_models.py
@@ -0,0 +1,7 @@
+import printing_functions
+
+unprinted_designs = ['pokrowiec','samolot','statek','kostka rubika']
+completed_models = []
+
+printing_functions.print_models(unprinted_designs, completed_models)
+printing_functions.show_completed_models(completed_models)
