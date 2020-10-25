b955962a65b0e1e09a11eaacf23a8b50d0783b0a
Łukasza Abramczyk <71088814+abramspl@users.noreply.github.com>
2020-10-25 08:23:36 +0100

    funkcja do programu koszulka_program

---
+++ koszulka_funkcja.py
@@ -0,0 +1,4 @@
+def koszulka(rozmiar, tekst):
+    """"Wyswietla wybrany rozmiar koszulki oraz tekst nadruku"""
+    print(f'\nTwoja wielkosc koszulki to: {rozmiar.upper()}')
+    print(f'Oto tekst nadruku na koszulce:\n--- {tekst.upper()} ---')
