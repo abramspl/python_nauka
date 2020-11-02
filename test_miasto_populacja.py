9de725943aa0da9d80afba47a325c71b36496a12
Łukasza Abramczyk <71088814+abramspl@users.noreply.github.com>
2020-11-02 14:13:00 +0100

    testowanie kodu - testowanie funkcji

---
+++ test_miasto_populacja.py
@@ -0,0 +1,17 @@
+import unittest
+from populacja import miasto_funkcja
+
+class TestMiastaDane(unittest.TestCase):
+    """Test dla programu 'populacja.py'."""
+
+    def test_miasto_panstwo(self):
+        """Czy dane 'Warszawa Polska' sa prawidlowo obslugiwane"""
+        dane = miasto_funkcja('warszawa','polska')
+        self.assertEqual(dane, 'Warszawa, Polska.')
+
+    def test_miasto_populacja(self):
+        dane = miasto_funkcja('warszawa','polska','45 000 000')
+        self.assertEqual(dane, 'Warszawa, Polska - Populacja 45 000 000.')
+
+if __name__ == '__main__':
+    unittest.main()
